ea83660f63a81c4b976cf9f971a6d21370d2d7a6
isadora <141033663+isabernegozzi@users.noreply.github.com>
2025-08-08 07:46:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -36,3 +36,21 @@ function gerarSenha() {
     }
     campoSenha.value = senhaGerada;
 }
+function classificaSenha(tamanhoAlfabeto) {
+  let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+  console.log(entropia);
+
+  forcaSenha.classList.remove("fraca", "media", "forte");
+  if (entropia > 57) {
+    forcaSenha.classList.add("forte");
+  } else if (entropia > 35 && entropia <= 57) {
+    forcaSenha.classList.add("media");
+  } else if (entropia <= 35) {
+    forcaSenha.classList.add("fraca");
+  }
+  const valorEntropia = document.getElementById("entropia");
+  valorEntropia.textContent = (
+    2 ** Math.floor(entropia) /
+    (100e6 * 60 * 60 * 24)
+  ).toFixed(2);
+}
